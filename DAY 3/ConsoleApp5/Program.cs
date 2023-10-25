c06e081dbee72c73639409d095bd200271d5ed96
sushil <147249755+Sushilgavit@users.noreply.github.com>
2023-10-25 14:50:37 +0530

    DAY_3_assignment

---
+++ DAY 3/ConsoleApp5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class student
+    {
+        string name;
+        int math;
+        int science;
+        int eng;  //int total ;
+        int mark;
+
+
+        public student(string name, int math, int science, int eng)
+        {
+            this.name = name;
+            this.math = math;
+            this.science = science;
+            this.eng = eng;
+            this.mark = mark;
+        }
+
+        public int TotalMarks()
+        {
+            return mark = math + science + eng;
+        }
+
+        public void display()
+        {
+            TotalMarks();
+            Console.WriteLine($"name = {name} mark ={mark}");
+        }
+    }
+    public class main
+    {
+        static void Main(string[] args)
+        {
+            student s = new student("sushil", 100, 100, 100);
+            s.display();
+        }
+
+
+    }
+}
