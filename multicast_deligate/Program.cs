f01f2dd4a1144a9739fde9d6b6269dfe0c7bd59f
sushil <147249755+Sushilgavit@users.noreply.github.com>
2023-11-01 09:50:45 +0530

    Calculator

---
+++ multicast_deligate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+delegate void math(float a , float b);
+
+/*add and multipl two no using multicast deligate with anonymous method */
+class Sushil
+{
+    static void Main()
+    {
+
+        math count = delegate (float a, float b)
+        {
+            float sum = a + b;
+            Console.WriteLine("additionn is :"+sum);
+        };
+
+        count += delegate (float a, float b)
+                {
+                    float multi = a * b;
+                    Console.WriteLine("multipictaion is :" +multi);
+                };
+
+        count += delegate (float a, float b)
+        {
+            float div = a / b;
+            Console.WriteLine("div is :" + div);
+        };
+
+        count(11,22);
+        Console.WriteLine();
+
+    }
+
+}
