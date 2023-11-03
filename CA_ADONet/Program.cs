49386dce8f83d183c33b56f4c5b73be1a4f67059
Sushil <147249755+Sushilgavit@users.noreply.github.com>
2023-11-03 15:54:18 +0530

    ADO.net project

---
+++ CA_ADONet/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CA_ADONet
+{
+    internal class Program
+    {   private static IConfiguration _iconfiguration;
+        static void Main(string[] args)
+        {
+            GetAppSettingsFile();
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            PrintProduct();
+        }
+
+        static void GetAppSettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("app_settings.json", optional: false, reloadOnChange: true);
+            _iconfiguration = builder.Build();
+        }
+
+        static void PrintProduct()
+        {
+            Productlayer obj = new Productlayer(_iconfiguration);
+            obj.Products();
+
+
+        }
+    }
+}
