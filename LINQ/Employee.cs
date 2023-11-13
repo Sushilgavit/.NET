1762bc4d3bb1499f3191aabbcfd331838364087f
Sushil <147249755+Sushilgavit@users.noreply.github.com>
2023-11-13 17:27:17 +0530

    LINQ (JOIN)

---
+++ LINQ/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+  public class Employee
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        public double Salary { get; set; }
+        public int DetId { get; set; }
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Linq;
+
+namespace LINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[6]
+         {
+            new Employee { Id = 1, EmpName = "John", Salary = 50000, DetId = 101 },
+            new Employee { Id = 2, EmpName = "Alice", Salary = 60000, DetId = 102 },
+            new Employee { Id = 3, EmpName = "Bob", Salary = 55000, DetId = 101 },
+            new Employee { Id = 4, EmpName = "Eva", Salary = 70000, DetId = 103 },
+            new Employee { Id = 5, EmpName = "Charlie", Salary = 45000, DetId = 102 },
+            new Employee { Id = 6, EmpName = "Grace", Salary = 80000, DetId = 103 }
+         };
+
+            // Creating an array of Department objects
+            Department[] departments = new Department[3]
+            {
+            new Department { DetId = 101, DeptName = "ADV" },
+            new Department { DetId = 102, DeptName = "HR" },
+            new Department { DetId = 103, DeptName = "ADMIN" }
+            };
+
+            var query = employees
+          .Join(departments, emp => emp.DetId, dept => dept.DetId, (emp, dept) => new { emp.EmpName, dept.DeptName });
+
+            foreach (var result in query)
+            {
+                Console.WriteLine($"Employee: {result.EmpName}, Department: {result.DeptName}");
+            }
+        }
+    }
+}
